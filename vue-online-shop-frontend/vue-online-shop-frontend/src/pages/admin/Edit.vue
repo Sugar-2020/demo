4c3ce142b2c37fd2399ff433ea556e7da44c9c34
Sugar-2020 <1393672101@qq.com>
2020-08-11 00:38:05 +0800

    页面跳转

---
+++ vue-online-shop-frontend/vue-online-shop-frontend/src/pages/admin/Edit.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <div class="title">
+            <h1>This is Admin/Edit/{{$route.params.id}}/{{$route.params.name}}/{{$route.path}}/{{$route.name}}</h1>
+        </div>
+    </div>
+</template>
